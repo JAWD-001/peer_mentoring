9a49d2f2e4c3c8434c3d5c11a7f9438fa23d0f63
James Dycus <dycus.j.jd@gmail.com>
2023-02-16 15:48:05 -0500

    added admin registration for all models in account management app

--- peer_mentoring/account_management/admin.py
+++ peer_mentoring/account_management/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Interest, Photo, CustomUser, UserProfile
 
 # Register your models here.
+admin.site.register(Interest)
+admin.site.register(Photo)
+admin.site.register(CustomUser)
+admin.site.register(UserProfile)
