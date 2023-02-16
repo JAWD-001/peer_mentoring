2b7b2887511eeca542f029e794b8701b6552ca6e
James Dycus <dycus.j.jd@gmail.com>
2023-02-16 16:20:48 -0500

    added admin registration for group models

--- peer_mentoring/groups/admin.py
+++ peer_mentoring/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Avatar, Category, Group
+
 # Register your models here.
+
+admin.sites.register(Avatar)
+admin.sites.register(Category)
+admin.sites.register(Group)
