96c35255e73c7eb8f332f3127136e37387102409
James Dycus <dycus.j.jd@gmail.com>
2023-02-20 23:34:49 -0500

    added views and settings information

--- peer_mentoring/account_management/views.py
+++ peer_mentoring/account_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetCompleteView, PasswordResetConfirmView, LogoutView
+
+from .forms import CreateUserForm
 # Create your views here.
+
+class CreateUserView(CreateView):
+    form_class = CreateUserForm
--- peer_mentoring/peer_mentoring/settings.py
+++ peer_mentoring/peer_mentoring/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGOUT_REDIRECT_URL = '/login/'
+
+AUTH_USER_MODEL = 'account_management.CustomUsers'
