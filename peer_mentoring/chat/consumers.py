141c17a484ccc70831763cef7a6c4763407ea6d6
James Dycus <dycus.j.jd@gmail.com>
2023-03-16 10:35:44 -0400

    edited consumers to change to group_id

--- peer_mentoring/chat/consumers.py
+++ peer_mentoring/chat/consumers.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope["user"]
-        self.id = self.scope["url_route"]["kwargs"]["course_id"]
+        self.user = self.scope["userprofile"]
+        self.id = self.scope["url_route"]["kwargs"]["group_id"]
         self.room_group_name = f"chat_{self.id}"
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
