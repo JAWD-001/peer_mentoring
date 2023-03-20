c1cf909f8ce51fb6201686d02b3ec545cd598b48
James Dycus <dycus.j.jd@gmail.com>
2023-03-20 00:48:49 -0400

    added function based views for groups list, detail, and members index

--- peer_mentoring/groups/views.py
+++ peer_mentoring/groups/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
+
+from peer_mentoring.account_management.models import UserProfile
+
+from .models import Group
 
 # Create your views here.
 
 
-def group_view(request):
-    pass
+@login_required
+def group_index_view(request):
+    groups = Group.objects.all()
+    context = {"groups": groups}
+    return render(request, "groups_index.html", context)
+
+
+@login_required
+def group_view(request, detail_id):
+    group = get_object_or_404(Group, pk=detail_id)
+    context = {
+        "group": group,
+    }
+    return render(request, "group_detail.html", context)
+
+
+@login_required
+def group_members_index_view(request):
+    members = UserProfile.objects.all()
+    context = {"members": members}
+    return render(request, "group_member_index", context)
