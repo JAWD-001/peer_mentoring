076f05adcbdc25fa089afbe735af3d025267b62a
James Dycus <dycus.j.jd@gmail.com>
2023-03-20 01:19:08 -0400

    added urlpatterns for groups

--- peer_mentoring/groups/urls.py
+++ peer_mentoring/groups/urls.py
@@ -1,6 +1,20 @@
-from django.contrib import admin
 from django.urls import path
 
+from peer_mentoring.groups.views import (
+    group_index_view,
+    group_members_index_view,
+    group_view,
+)
+
+app_name = "groups"
+
 urlpatterns = [
-    #path('')
+    path("", group_index_view, name="groups_home"),
+    path("group_detail/<int:pk>", group_view, name="group_detail"),
+    path(
+        "group_members/",
+        group_members_index_view,
+        name="group_members",
+        name="group_members",
+    ),
 ]
