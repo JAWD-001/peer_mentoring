8042b75f2ad4a612a77ee12311b393b2ec2bf8d9
James Dycus <dycus.j.jd@gmail.com>
2023-03-27 15:09:13 -0400

    changed to include form in the context dict.

--- peer_mentoring/groups/views.py
+++ peer_mentoring/groups/views.py
@@ -21,21 +21,21 @@ def group_detail(request, pk):
     group = get_object_or_404(Group, pk=pk)
     post = Post.objects.all(pk=group.id)
     member = UserProfile.objects.all()
+    form = GroupPostForm(request.POST)
     context = {
         "group": group,
         "post": post,
         "members": member,
+        "form": form,
     }
     if request.metthod == "POST":
-        form = GroupPostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.group = group
             post.author = request.user
             post.save()
             messages.success(request, "Post Added!")
-            form = GroupPostForm()
-            return render(request, "group_detail.html", context, form)
+            return render(request, "group_detail.html", context)
         else:
             form = GroupPostForm()
         return render(request, "group_detail.html", {"form": form})
@@ -45,20 +45,20 @@ def group_detail(request, pk):
 def group_show_post(request, post_id):
     post = Post.objects.get(pk=post_id)
     comment = Comment.objects.all(pk=post.id)
+    form = GroupPostCommentForm(request.POST)
     context = {
         "post": post,
         "comment": comment,
+        "form": form,
     }
     if request.method == "POST":
-        form = GroupPostCommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             post.author = request.user
             post.save()
             messages.success(request, "Post Added!")
-            form = GroupPostCommentForm()
-            return render(request, "group_show_post.html", context, form)
+            return render(request, "group_show_post.html", context)
     else:
         form = GroupPostCommentForm()
     return render(request, "group_show_post.html", {"form": form})
