b42843416d970b95fb52d8f86736ed9da7ed4789
James Dycus <dycus.j.jd@gmail.com>
2023-03-28 13:13:13 -0400

    added url route for create group

--- peer_mentoring/groups/urls.py
+++ peer_mentoring/groups/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import create_group_post, group_detail, group_index, group_show_post
+from .views import (
+    create_group,
+    create_group_post,
+    group_detail,
+    group_index,
+    group_show_post,
+)
 
 app_name = "groups"
 
 urlpatterns = [
     path("", group_index, name="groups_home"),
+    path("create_group", create_group, name="create_group"),
     path("<int:group_id>/", group_detail, name="group_detail"),
     path("<int:group_id>/posts/<int:post_id>/", group_show_post, name="show_post"),
     path("<int:group_id>/create", create_group_post, name="create_post"),
