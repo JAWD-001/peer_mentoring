298e9ce27b97f93e96d7e70686b6f4e717ca30de
James Dycus <dycus.j.jd@gmail.com>
2023-05-02 11:49:42 -0400

    added add_friend fbv, need to consolidate into the user_profile view potentially. Also, please check the groups.models code. I added the members and moderators fields, but having a default value error when creating a migration. Need assistances with what default should be

--- peer_mentoring/account_management/models.py
+++ peer_mentoring/account_management/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-#  from groups.models import Group
-
 
 # Create your models here.
 class Interest(models.Model):
