2f9f4266a20ceba91ce6c34b47e4d8ad6e3d7798
James Dycus <dycus.j.jd@gmail.com>
2023-05-22 00:19:52 -0400

    added urls for all new views

--- peer_mentoring/groups/urls.py
+++ peer_mentoring/groups/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
 from .views import (
+    ban_user,
+    delete_comment,
+    delete_post,
     group_detail,
     group_index,
     group_show_post,
@@ -19,5 +22,18 @@ urlpatterns = [
     path("groups_joined/", groups_joined, name="groups_joined"),
     path("groups_moderated/", groups_moderated, name="groups_moderated"),
     path("<int:group_id>/", group_detail, name="group_detail"),
-    path("<int:group_id>/posts/<int:post_id>/", group_show_post, name="show_post"),
+    path(
+        "group/<int:group_id>/posts/<int:post_id>/", group_show_post, name="show_post"
+    ),
+    path("group/<int:group_id>/ban_user/<int:user_id>/", ban_user, name="ban_user"),
+    path(
+        "group/<int:group_id>/delete_post/<int:post_id/>",
+        delete_post,
+        name="delete_post",
+    ),
+    path(
+        "group/<int:group_id>/post/<int:post_id>/delete_comment/<int:comment_id>/",
+        delete_comment,
+        name="delete_comment",
+    ),
 ]
