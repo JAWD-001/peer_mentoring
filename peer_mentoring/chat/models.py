b6133a61cde2202b83df88e1a79119f9453b4552
James Dycus <dycus.j.jd@gmail.com>
2023-06-16 10:25:43 -0400

    added model field options for message on ChatMessage.

--- peer_mentoring/chat/models.py
+++ peer_mentoring/chat/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class ChatMessage(models.Model):
     id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    message = models.TextField()
+    message = models.TextField(max_length=300, blank=False, null=False)
     added = models.DateTimeField(auto_now_add=True, null=False, blank=False)
