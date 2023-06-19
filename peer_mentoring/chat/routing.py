a11bebdf86ea5f4885b781ebc4e85e8cbc816a4f
James Dycus <dycus.j.jd@gmail.com>
2023-06-19 15:46:01 -0400

    added button to link to private chat in other_user_profile template.

--- peer_mentoring/chat/routing.py
+++ peer_mentoring/chat/routing.py
@@ -6,6 +6,6 @@ websocket_urlpatterns = [
     re_path(r"ws/chat/room/(?P<group_id>\d+)/$", consumers.ChatConsumer.as_asgi()),
     re_path(
         r"ws/chat/private/(?P<receiver_id>\d+)/$",
-        consumers.PrivateChatMessage.as_asgi(),
+        consumers.PrivateChatConsumer.as_asgi(),
     ),
 ]
