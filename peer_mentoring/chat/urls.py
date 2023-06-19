3e970922ff19aeefaed8ad49e43dbd3285623a07
James Dycus <dycus.j.jd@gmail.com>
2023-06-19 12:54:12 -0400

    added url for private chats

--- peer_mentoring/chat/urls.py
+++ peer_mentoring/chat/urls.py
@@ -5,7 +5,11 @@ from . import views
 app_name = "chat"
 
 urlpatterns = [
-    path("room/<int:group_id>/", views.group_chat_room, name="group_chat_room"),
+    path("room/group/<int:group_id>/", views.group_chat_room, name="group_chat_room"),
     # TODO: private chat
-    # path("room/<int:sender>/<int:receiver>", views.private_group_chat_room, name="private_chat_room",),
+    path(
+        "room/sender/<int:sender>/receiver/<int:receiver>",
+        views.private_group_chat_room,
+        name="private_chat_room",
+    ),
 ]
