939caf2995937701c6a7aee378a5756193db4b33
James Dycus <dycus.j.jd@gmail.com>
2023-06-27 14:53:31 -0400

    added tests for group views with some fixtures for posts and comments.

--- peer_mentoring/tests/test_groups.py
+++ peer_mentoring/tests/test_groups.py
@@ -5,13 +5,15 @@ from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from django.test import Client
 from django.urls import reverse
-from groups.models import Avatar, Category, Group
+from groups.models import Avatar, Category, Comment, Group, Post
 from PIL import Image
 
 # from groups.views import group_index, groups_moderated
 
 
-# Fixture creates avatar instance
+"""Fixtures"""
+
+
 @pytest.fixture
 def avatar(db):
     avatar = Avatar(name="Test Avatar")
@@ -57,6 +59,26 @@ def group(db, avatar, category, user):
     )
     group.members.add(user)
     return group
+
+
+# Fixture to make post instance
+@pytest.fixture
+def post(db, user, group):
+    return Post.objects.create(
+        author=user, title="Test Post Title", content="Test Post Content", group=group
+    )
+
+
+@pytest.fixture
+def comment(db, user, post):
+    return Comment.objects.create(
+        author=user,
+        content="Test",
+        post=post,
+    )
+
+
+"""View Tests"""
 
 
 def test_group_title(group):
@@ -106,6 +128,7 @@ def test_groups_joined_authenticated(client_authenticated, user, group):
     assert group in response.context["groups"]  # noqa: S101
 
 
+# tests groups_joined_unauthenticated
 @pytest.mark.django_db
 def test_groups_joined_unauthenticated(client, user, group):
     # Add the user to the group's members
@@ -121,6 +144,81 @@ def test_groups_joined_unauthenticated(client, user, group):
     assert "login" in response.url  # noqa: S101
 
 
+# test groups_moderated view
 @pytest.mark.django_db
-def test_groups_moderated_view():
-    pass
+def test_groups_moderated(client_authenticated, user, group):
+    # Send a GET request to the view
+    response = client_authenticated.get(reverse("groups:groups_moderated"))
+
+    # Assert the response status code is 200 (OK)
+    assert response.status_code == 200  # noqa: S101
+
+    # Assert the user's group is in the response context
+    assert group in response.context["groups"]  # noqa: S101
+
+    # Assert the response contains the title of the group
+    assert group.title in str(response.content)  # noqa: S101
+
+
+def test_group_detail_GET(client_authenticated, user, group):
+    # Add the user to the group's members
+
+    group.members.add(user)
+
+    # Send a GET request to the view
+    response = client_authenticated.get(
+        reverse("groups:group_detail", kwargs={"group_id": group.id})
+    )
+
+    # Assert the response status code is 200 (OK)
+    assert response.status_code == 200  # noqa: S101
+
+    # Assert that user in group members
+    assert user in group.members.all()  # noqa: S101
+
+    # Assert the response contains the title of the group
+    assert group.title in str(response.content)  # noqa: S101
+
+
+@pytest.mark.django_db
+def test_group_detail_POST(client_authenticated, post):
+    # Add the user to the post's group members
+    post.group.members.add(post.author)
+
+    # Send a POST request to the view
+    response = client_authenticated.post(
+        reverse("groups:group_detail", kwargs={"group_id": post.group.id}),
+        data={"title": post.title, "content": post.content},
+    )
+
+    # Assert the response status code is 302 (Redirection)
+    assert response.status_code == 302  # noqa: S101
+
+    # Assert a new post has been added to the group
+    assert Post.objects.filter(  # noqa: S101
+        group=post.group, content=post.content, title=post.title
+    ).exists()
+
+
+@pytest.mark.django_db
+def test_comment_created(comment):
+    assert comment.content == "Test"  # noqa: S101
+
+
+def test_group_show_post_GET(client_authenticated, user, group, post, comment):
+    url = reverse("groups:show_post", kwargs={"group_id": group.id, "post_id": post.id})
+    response = client_authenticated.get(url)
+    assert response.status_code == 200  # noqa: S101
+    assert post == response.context["post"]  # noqa: S101
+    assert comment in response.context["comments"]  # noqa: S101
+
+
+@pytest.mark.django_db
+def test_group_show_post_POST(client_authenticated, user, group, post):
+    url = reverse("groups:show_post", kwargs={"group_id": group.id, "post_id": post.id})
+    comment_data = {"content": "Test comment."}
+    response = client_authenticated.post(url, comment_data)
+    assert response.status_code == 302  # noqa: S101
+    assert Comment.objects.filter(  # noqa: S101
+        content="Test comment.", author=user, post=post
+    ).exists()
