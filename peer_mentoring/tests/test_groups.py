b0a450f2e076bc5f4258ad7f47bdc6e7b762dace
James Dycus <dycus.j.jd@gmail.com>
2023-06-26 20:38:48 -0400

    added tests to test gorup index get and post requests in view. Also added client authentication fixture.

--- peer_mentoring/tests/test_groups.py
+++ peer_mentoring/tests/test_groups.py
@@ -1,16 +1,17 @@
 import io
 
 import pytest
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
+from django.test import Client
+from django.urls import reverse
 from groups.models import Avatar, Category, Group
 from PIL import Image
 
-User = get_user_model()
-
-# from groups.views import groups_moderated
+# from groups.views import group_index, groups_moderated
 
 
+# Fixture creates avatar instance
 @pytest.fixture
 def avatar(db):
     avatar = Avatar(name="Test Avatar")
@@ -24,16 +25,27 @@ def avatar(db):
     return avatar
 
 
+# Fixture creates a category instance
 @pytest.fixture
 def category(db):
     return Category.objects.create(name="Test Category")
 
 
+# Fixture creates a user instance
 @pytest.fixture
 def user(db):
     return User.objects.create_user(username="testuser", password="12345")  # noqa: S106
 
 
+# fixture handles client authentication
+@pytest.fixture
+def client_authenticated(user):
+    client = Client()
+    client.login(username="testuser", password="12345")  # noqa: S106
+    return client
+
+
+# Fixture creates a group instance
 @pytest.fixture
 def group(db, avatar, category, user):
     group = Group.objects.create(
@@ -53,6 +65,27 @@ def test_group_title(group):
 
 def test_group_can_join(group, user):
     assert not group.can_join(user)  # noqa: S101
+
+
+def test_group_index_GET(client_authenticated):
+    response = client_authenticated.get(reverse("groups:group_home"))
+    assert response.status_code == 200  # noqa: S101
+    assert "form" in response.context  # noqa: S101
+    assert "groups" in response.context  # noqa: S101
+
+
+def test_group_index_POST(client_authenticated, user, avatar, category):
+    new_group = {
+        "title": "Test Group",
+        "avatar": avatar.id,  # use a valid id for fk  noqa: S101
+        "description": "Test Description",
+        "category": category.id,  # use a valid id for fk  noqa: S101
+    }
+    response = client_authenticated.post(reverse("groups:group_home"), data=new_group)
+    assert response.status_code == 302  # Check redirect  # noqa: S101
+    assert Group.objects.filter(  # noqa: S101
+        title="Test Group"
+    ).exists()  # Check if the group was created  # noqa: S101
 
 
 @pytest.mark.django_db
