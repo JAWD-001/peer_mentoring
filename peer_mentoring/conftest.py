92ad9bdc70f7b6bf137538f121153674234ee8c9
James Dycus <dycus.j.jd@gmail.com>
2023-07-07 17:54:56 -0400

    added tests for account_management, updated url and view

--- peer_mentoring/conftest.py
+++ peer_mentoring/conftest.py
@@ -1,4 +1,5 @@
 import io
+from io import BytesIO
 
 import pytest
 from account_management.models import (
@@ -9,6 +10,7 @@ from account_management.models import (
     UserProfile,
 )
 from django.core.files.base import ContentFile
+from django.core.files.images import ImageFile
 from django.test import Client
 from groups.models import Avatar, Category, Comment, Group, GroupJoinRequest, Post
 from PIL import Image
@@ -20,6 +22,24 @@ from tests.factories import UserFactory
 """
 Fixtures
 """
+
+
+@pytest.fixture
+def user(db):
+    return UserFactory.create()
+
+
+@pytest.fixture
+def user2(db):
+    return UserFactory.create()
+
+
+@pytest.fixture
+def interest(db):
+    return Interest.objects.create(
+        name="Interest1",
+        image=ContentFile(b"image_content", name="test.png"),
+    )
 
 
 # Fixture creates an avatar instance for group fixture
@@ -40,17 +60,6 @@ def avatar(db):
 @pytest.fixture
 def category(db):
     return Category.objects.create(name="Test Category")
-
-
-# Creates user instance
-@pytest.fixture
-def user(db):
-    return UserFactory.create()
-
-
-@pytest.fixture
-def user2(db):
-    return UserFactory.create()
 
 
 # fixture handles client authentication
@@ -107,37 +116,40 @@ def comment(db, user, post):
 
 
 @pytest.fixture
-def interest(db):
-    return Interest.objects.create(
-        name="Interest1",
-        image=ContentFile(b"image_content", name="test.png"),
-    )
-
-
-@pytest.fixture
 def photo(db, user):
-    photo = Photo(name="Test Photo")
-    img = Image.new("RGB", (60, 30), color=(73, 109, 137))  # creating a dummy image
-    img_file = io.BytesIO()
-    img.save(img_file, format="JPEG")
-    img_file.name = "test_photo.jpg"
-    img_file.seek(0)
-    photo.image.save(img_file.name, ContentFile(img_file.read()))
-    photo.save()
-    return photo(
-        image=photo,
+    # create an in-memory image file with Pillow
+    image_file = BytesIO()
+    Image.new("RGB", (100, 100)).save(image_file, "JPEG")
+    image_file.seek(0)
+
+    photo = Photo.objects.create(
+        image=ImageFile(image_file, name="test.jpg"),
         description="A cool photo",
         user=user,
     )
+    return photo
 
 
 @pytest.fixture
 def user_profile(db, user, photo, interest):
-    profile = UserProfile.objects.create(
-        user=user, avatar=photo, title="Some Title", bio="This is a bio"
-    )
-    profile.interests.add(interest)
-    return profile
+    user_profile = UserProfile.objects.get(user=user)
+    user_profile.avatar = photo
+    user_profile.title = "Some Title"
+    user_profile.bio = "This is a bio"
+    user_profile.interests.add(interest)
+    user_profile.save()
+    return user_profile
+
+
+@pytest.fixture
+def user_profile2(db, user2, photo, interest):
+    user_profile2 = UserProfile.objects.get(user=user2)
+    user_profile2.avatar = photo
+    user_profile2.title = "Some Title"
+    user_profile2.bio = "This is a bio"
+    user_profile2.interests.add(interest)
+    user_profile2.save()
+    return user_profile
 
 
 @pytest.fixture
--- peer_mentoring/tests/test_account_management.py
+++ peer_mentoring/tests/test_account_management.py
@@ -1,1 +1,100 @@
-# import pytest
+import pytest
+from account_management.forms import AddPhotoForm, CustomUserChangeForm
+from account_management.models import Photo
+from django.contrib.messages import get_messages
+from django.http import request
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_profile_home_get(client_authenticated, user):
+    response = client_authenticated.get(reverse("account_management:profile_home"))
+    assert response.status_code == 200  # noqa: S101
+    assert isinstance(response.context["form"], CustomUserChangeForm)  # noqa: S101
+    assert isinstance(response.context["photo_upload"], AddPhotoForm)  # noqa: S101
+    assert response.context["user"] == user  # noqa: S101
+
+
+@pytest.mark.django_db
+def test_profile_home_post_photo(client_authenticated, user, photo):
+    assert (  # noqa: S101
+        photo.description is not None
+    ), "Photo instance doesn't have a description!"  # noqa: S101
+
+    with open(photo.image.path, "rb") as img_file:
+        Photo.objects.all().delete()
+
+        response = client_authenticated.post(
+            reverse("account_management:profile_home"),
+            data={"image": img_file, "description": photo.description},
+            format="multipart",
+        )
+
+    assert response.status_code == 200  # noqa: S101
+    assert (  # noqa: S101
+        Photo.objects.count() == 1
+    ), "Expected one Photo object after the POST request!"
+
+    new_photo = Photo.objects.first()
+    assert (  # noqa: S101
+        new_photo.description == photo.description
+    ), "Description of the newly added photo doesn't match with the provided one!"
+
+    assert "Photo Added!" in list(  # noqa: S101
+        map(str, list(get_messages(response.wsgi_request)))
+    )
+
+
+@pytest.mark.django_db
+def test_profile_home_post_update_profile(client_authenticated, user_profile):
+    form_data = {
+        "title": "Some Title",
+        "bio": "This is a bio",
+    }
+    response = client_authenticated.post(
+        reverse("account_management:profile_home"), data=form_data
+    )
+    user_profile.refresh_from_db()
+    assert response.status_code == 200  # noqa: S101
+    assert user_profile.title == "Some Title"  # noqa: S101
+    assert user_profile.bio == "This is a bio"  # noqa: S101
+
+
+@pytest.mark.django_db
+def test_view_profile(client_authenticated, user_profile, user):
+    response = client_authenticated.get(
+        reverse("account_management:view_profile", kwargs={"user_id": user.id})
+    )
+
+    assert response.status_code == 200  # noqa: S101
+    assert "user" in response.context  # noqa: S101
+    assert response.context["user"] == user  # noqa: S101
+
+
+def test_user_index(client_authenticated, user_profile):
+    response = client_authenticated.get(reverse("account_management:user_index"))
+
+    assert response.status_code == 200  # noqa: S101
+    assert "profiles" in response.context  # noqa: S101
+    for profile in response.context["profiles"]:
+        assert profile != request.user  # noqa: S101
+
+
+def test_mentor_index(client_authenticated, user_profile, user):
+    response = client_authenticated.get(reverse("account_management:mentor_index"))
+
+    assert response.status_code == 200  # noqa: S101
+    assert "mentors" in response.context  # noqa: S101
+    for mentor in response.context["mentors"]:
+        assert mentor != request.user  # noqa: S101
+
+
+def test_send_friend_request(client_authenticated, user_profile, user):
+    response = client_authenticated.post(
+        reverse("account_management:send_request", kwargs={"user_id": user.id})
+    )
+
+    assert response.status_code == 302  # noqa: S101
+    assert response.url == reverse(  # noqa: S101
+        "account_management:view_profile", kwargs={"user_id": user.id}
+    )
