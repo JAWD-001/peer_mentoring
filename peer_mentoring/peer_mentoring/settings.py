7e24e0b7cc97ad3a7049ab25f8329edddcafe678
James Dycus <dycus.j.jd@gmail.com>
2023-07-20 00:49:41 -0400

    added dockerfile and docker-compose

--- peer_mentoring/peer_mentoring/settings.py
+++ peer_mentoring/peer_mentoring/settings.py
@@ -98,6 +98,7 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
