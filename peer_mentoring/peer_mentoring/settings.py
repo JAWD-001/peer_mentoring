4c6df2707c581cfdbb6c2fd2edae0c0d4bb81841
James Dycus <dycus.j.jd@gmail.com>
2023-07-20 01:14:35 -0400

    added docker-compose configs and added postgres settings configs with .env variables in settings.py

--- peer_mentoring/peer_mentoring/settings.py
+++ peer_mentoring/peer_mentoring/settings.py
@@ -98,6 +98,17 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT", cast=int),
+    }
+}
+
 
 DATABASES = {
     "default": {
