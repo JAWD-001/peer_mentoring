cb64775e191fa98caa682ba31c0f4b86d255ae38
James Dycus <dycus.j.jd@gmail.com>
2023-07-26 02:54:34 -0400

    added dockerfile and docker-compose configurations for static and media files. Also added running migrations in entrypoint.prod

--- peer_mentoring/peer_mentoring/settings.py
+++ peer_mentoring/peer_mentoring/settings.py
@@ -91,26 +91,28 @@ ASGI_APPLICATION = "peer_mentoring.asgi.application"
 
 
 # redis for development
+"""
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {"hosts": [("127.0.0.1", 6379)]},
     },
 }
+"""
 
 # redis for deployment
-"""
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {"hosts": [("redis", 6379)]},
     },
 }
-"""
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 # sqlite for development
 DATABASES = {
     "default": {
@@ -118,9 +120,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+"""
 
 # postgres for deployment
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": config("SQL_ENGINE", "django.db.backends.sqlite3"),
@@ -131,7 +134,7 @@ DATABASES = {
         "PORT": config("SQL_PORT", "5432"),
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- peer_mentoring/peer_mentoring/urls.py
+++ peer_mentoring/peer_mentoring/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path("", include("account_management.urls")),
 ]
 
-if settings.DEBUG:
+if bool(settings.DEBUG):
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
