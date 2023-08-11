ecb8d378cf6961a86dfc1503c1481b491fac65b8
Bob Belderbos <bobbelderbos@gmail.com>
2023-08-11 19:09:34 +0200

    upgrade to postgres

--- peer_mentoring/settings.py
+++ peer_mentoring/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,12 +98,10 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
